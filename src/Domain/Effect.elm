4633c35cb38665586d8d2f84845c48103db9a00d
Nik <sharma7n@gmail.com>
2020-04-19 16:01:04 +0000

    working on effect

--- src/Domain/Effect.elm
+++ src/Domain/Effect.elm
@@ -1,6 +1,34 @@
-module Domain.Effect exposing (Effect(..))
+module Domain.Effect exposing
+    ( BattleEffect(..)
+    , DungeonEffect(..)
+    , Effect(..)
+    , GlobalEffect(..)
+    , HomeEffect(..)
+    )
 
 
 type Effect
+    = Global GlobalEffect
+    | Home HomeEffect
+    | Dungeon DungeonEffect
+    | Battle BattleEffect
+
+
+type GlobalEffect
+    = ChangeHitPoints Int
+    | ChangeGold Int
+
+
+type HomeEffect
     = ChangeTime Int
-    | ChangeHitPoints Int
+
+
+type DungeonEffect
+    = ChangeSafety Int
+    | ChangePath Int
+
+
+type BattleEffect
+    = ChangeActionPoints Int
+    | GenerateBlock Int
+    | DealDamage Int
