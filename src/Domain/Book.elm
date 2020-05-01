848f4e7031e2e81da185bf2d8d4aba3b722f5664
unknown <sharma7n@gmail.com>
2020-04-30 23:10:46 -0500

    working book

--- src/Domain/Book.elm
+++ src/Domain/Book.elm
@@ -1,1 +1,55 @@
-module Main exposing (..)
+module Domain.Book exposing
+    ( Book
+    , Genre(..)
+    , getById
+    )
+
+import Domain.Effect as Effect exposing (Effect)
+
+
+type Genre
+    = NonFiction
+    | Horror
+    | Mystery
+    | Romance
+    | Fantasy
+    | Comedy
+
+
+type alias Book =
+    { id : String
+    , title : String
+    , description : String
+    , author : String
+    , genre : Genre
+    , chapters : Int
+    , pages : Int
+    , effects : List Effect
+    }
+
+
+getById : String -> Maybe Book
+getById id =
+    case id of
+        "wizenGulch" ->
+            Just wizenGulch
+
+        _ ->
+            Nothing
+
+
+
+-- BOOK OBJECTS
+
+
+wizenGulch : Book
+wizenGulch =
+    { id = "wizenGulch"
+    , title = "Wizen Gulch"
+    , description = "A tale of gunpowder, gold, and goblins."
+    , author = "D. M."
+    , genre = Fantasy
+    , chapters = 10
+    , pages = 30
+    , effects = []
+    }
